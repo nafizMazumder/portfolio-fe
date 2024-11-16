56e9675b28af90e1698504d06403181bb7d858d4
nafiz <nafizuddin80@gmail.com>
2024-11-16 17:37:39 -0400

    updated post url again

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,7 +14,9 @@ const Contact = () => {
         e.preventDefault();
         setLoading(true);
         try {
-            const res = await axios.post(`${process.env.REACT_APP_API_URL}/contact`, formData);
+            //const res = await axios.post(`${process.env.REACT_APP_API_URL}/contact`, formData);
+            const res = await axios.post(`https://be-nafizmazumder.netlify.app/.netlify/functions/contact`, formData);
+
             setResponseMessage(res.data.message);
         } catch (error) {
             console.error(error);
