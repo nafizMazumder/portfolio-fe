702f782b3aa0089d150f8de609fc439f24a655c3
Nafiz Mazumder <nafizuddin80@gmail.com>
2024-11-17 18:57:24 -0400

    added background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,9 +29,13 @@ const Footer = () => {
     return (
         <footer>
             <p>&copy; {currentYear} Nafiz Mazumder.
-                <a href="https://www.flaticon.com/free-icons/developer"
+                <a href="https://www.flaticon.com/free-icon/developer_2572713?related_id=2572711&origin=search"
                    title="Favicon"
-                   target="_blank" rel="noopener noreferrer"> Favicon created by Pixel perfect - Flaticon.
+                   target="_blank" rel="noopener noreferrer"> Favicon by Pixel perfect - Flaticon.
+                </a>
+                <a href="https://www.freepik.com/free-vector/abstract-background-with-artificial-intelligence-elements_6082055.htm#fromView=search&page=3&position=19&uuid=04af831a-0e8d-4714-b0a3-b0d7b4699bc8&new_detail=true"
+                   title="Background"
+                   target="_blank" rel="noopener noreferrer"> Background by Freepik.
                 </a>
             </p>
             <ul>
